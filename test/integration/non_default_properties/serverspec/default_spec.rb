9ed872afdbd25b7380b5a0dcff13231c2f36449d
Wisen Tanasa <wisen@ceilfors.com>
2016-09-30 06:57:17 +0100

    Add more test.

--- test/integration/non_default_properties/serverspec/default_spec.rb
+++ test/integration/non_default_properties/serverspec/default_spec.rb
@@ -4,4 +4,8 @@ describe 'non_default_properties' do
   describe file('/tmp/gq') do
     it { should be_directory }
   end
+
+  describe file('/tmp/gq/branches/master') do
+    it { should be_directory }
+  end
 end
