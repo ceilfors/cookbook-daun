7ce9952a7139a1ca985338ba972ddc5363a16d02
Wisen Tanasa <wisen@ceilfors.com>
2016-10-07 07:12:12 +0100

    Fix rubocop.

--- resources/default.rb
+++ resources/default.rb
@@ -6,7 +6,7 @@ default_action :checkout
 
 action :checkout do
   daun = ::Daun::RuggedDaun.new(destination)
-  unless ::File.foreach("#{destination}/.git/config").any?{ |l| l[repository] }
+  unless ::File.foreach("#{destination}/.git/config").any? { |l| l[repository] }
     converge_by("Initialize daun repository at #{destination}") do
       daun.init repository
     end
@@ -14,10 +14,9 @@ action :checkout do
 
   config.each do |key, value|
     @repository = ::Rugged::Repository.init_at(destination)
-    if @repository.config[key] != value.to_s
-      converge_by("Update daun repository config '#{key}' to '#{value}'") do
-        @repository.config[key] = value
-      end
+    next if @repository.config[key] == value.to_s
+    converge_by("Update daun repository config '#{key}' to '#{value}'") do
+      @repository.config[key] = value
     end
   end
 
