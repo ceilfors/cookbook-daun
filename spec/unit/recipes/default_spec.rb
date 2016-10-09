78344d44c3801770c5799a9861d23d0c83352c6d
Wisen Tanasa <wisen@ceilfors.com>
2016-10-09 07:32:01 +0100

    Fix chefspec by stubbing Kernel.require method.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,6 +6,14 @@
 
 require 'spec_helper'
 
+module Kernel
+  alias :old_require :require
+  def require(path)
+    old_require(path) unless ['daun', 'rugged'].include?(path)
+  end
+end
+
+
 describe 'daun::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
